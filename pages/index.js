41c77b654559d0baf443d876d0bbcae7f85d8f86
Dalbosco <76927571+Dalboscogh@users.noreply.github.com>
2023-08-25 23:45:50 +0000

    adição da página indíce

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+export default function Home () {
+    return <h1>Te amo, meu amor. Sabe que desejo te dar todas as maravilhas do universo e compartilhar esse pequeno tempo que temos de vida.</h1>
+
+} 
