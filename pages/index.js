9429e92eeaeb6236e887e5aba939c43a4871d92f
Dalbosco <76927571+Dalboscogh@users.noreply.github.com>
2023-08-29 14:01:26 +0000

    adição de novos arquivos para criação do modelo para o app e ajustes na index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
-    <h1>
-      Te amo, Pamella, meu amor de minha vida. Sabe que desejo te dar todas as
-      maravilhas do universo e compartilhar esse pequeno tempo que temos de
-      vida.
-    </h1>
+    <div>
+      <span>
+        Esse site é dedicado para Pamella, minha esposa, parceira e amor de minha vida. Sabe que desejo te dar todas as
+        maravilhas do universo e compartilhar esse pequeno tempo que temos de
+        vida e sonhos. Obrigado pelo apoio de tornar isso real.
+      </span>
+      <div className="side-bar">
+        <a href="">Home</a>        
+        <a href="">Sobre mim</a>
+        <a href="">Flash-cards</a>
+        <a href="">Blog</a>
+
+      </div>
+    </div>
   );
 }
