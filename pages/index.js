ed2d2ec9c646de6e846d247341bc3b04358ce8c2
Dalbosco <76927571+Dalboscogh@users.noreply.github.com>
2023-08-29 14:14:14 +0000

    Ajustes na index principal - construção da página inicial e Home - apenas content

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,85 @@
-export default function Home() {
-  return (
-    <div>
-      <span>
-        Esse site é dedicado para Pamella, minha esposa, parceira e amor de minha vida. Sabe que desejo te dar todas as
-        maravilhas do universo e compartilhar esse pequeno tempo que temos de
-        vida e sonhos. Obrigado pelo apoio de tornar isso real.
-      </span>
-      <div className="side-bar">
-        <a href="">Home</a>        
-        <a href="">Sobre mim</a>
-        <a href="">Flash-cards</a>
-        <a href="">Blog</a>
+function App() {
+  const [message, setMessage] = useState("Esse site é dedicado para Pamella, minha esposa, parceira e amor de minha vida. Sabe que desejo te dar todas as maravilhas do universo e compartilhar esse pequeno tempo que temos de vida e sonhos. Obrigado pelo apoio de tornar isso real.");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const getLinkedInPublications = async () => {
+    const response = await axios.get("https://api.linkedin.com/v2/me/publications");
+    return response.data;
+  };
+
+  const getGitHubReadmeFiles = async () => {
+    const response = await axios.get("https://api.github.com/users/Dalboscogh/repos");
+    return response.data.map((repo) => repo.readme);
+  };
+
+  useEffect(() => {
+    getLinkedInPublications().then((publications) => setPublications(publications));
+    getGitHubReadmeFiles().then((readmeFiles) => setReadmeFiles(readmeFiles));
+  }, []);
+
+  const publications = useState([]);
+  const readmeFiles = useState([]);
+
+  return (
+    <div className="App">
+      <div className="message">
+        {message}
+      </div>
+
+      <div className="sidebar">
+        <button onClick={handleSidebarToggle}>
+          <i className="fa fa-bars"></i>
+        </button>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Sobre mim</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Log in/Sign up</a>
+          </li>
+          <li>
+            <a href="#">Flashcards</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={isSidebarOpen ? "main-content-open" : "main-content-closed"}>
+        <Home publications={publications} readmeFiles={readmeFiles} />
       </div>
     </div>
   );
 }
+
+const Home = ({ publications, readmeFiles }) => {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <h2>Publicações do LinkedIn</h2>
+      {publications.map((publication) => (
+        <div key={publication.id}>
+          <h3>{publication.title}</h3>
+          <p>{publication.description}</p>
+        </div>
+      ))}
+      <h2>Arquivos README do GitHub</h2>
+      {readmeFiles.map((readmeFile) => (
+        <div key={readmeFile.name}>
+          <h3>{readmeFile.name}</h3>
+          <p>{readmeFile.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
