fe7e7cc7748bba920e5371954d208ee5046efaa9
Dalbosco <76927571+Dalboscogh@users.noreply.github.com>
2023-09-04 03:09:35 +0000

    ajuste do import de _app.js de androgeno

--- pages/androgeno/_app.js
+++ pages/androgeno/_app.js
@@ -1,4 +1,4 @@
-import styles from "./pages/androgeno/styles.css";
+import styles from "../pages/androgeno/styles.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
